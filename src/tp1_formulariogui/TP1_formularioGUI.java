93435a1b38d7229b3e699929345df3b1fcc92f8d
Arian Perez <arianfelipeperez@gmail.com>
2024-08-26 15:39:22 -0300

    Initial commit

---
+++ src/tp1_formulariogui/TP1_formularioGUI.java
@@ -0,0 +1,18 @@
+
+package tp1_formulariogui;
+
+
+
+
+public class TP1_formularioGUI {
+
+   
+    public static void main(String[] args) {
+       
+        Vista vista = new Vista();
+        
+        vista.setVisible(true);
+        
+    }
+    
+}
